2347ac7212f254223ed59062fa6f6c7da208c2c6
Aelerinya <sarah.stecklinck@gmail.com>
2015-10-01 21:03:48 +0200

    Add simple home page
    Ajout d'une page d'acceuil toute simple, avec un lien vers le dépôt Github.
    
    Add a simple homepage, with a link to the GitHub repository.

---
+++ website/index.php
@@ -0,0 +1,12 @@
+<?php
+include_once('includes/html_header.php');
+include_once('includes/html_footer.php');
+
+html_header('acceuil');
+?>
+	<h2>Nix v2</h2>
+
+	<p>Version 2 de Nix, sources ici : <a href="https://github.com/Aelerinya/Nix_v2">Dépôt Github</a></p>
+<?php
+html_footer();
+?>
