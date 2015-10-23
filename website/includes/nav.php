7722daf99030028ce2ee3802ebd126a6a4d80a09
Seberus <jgonzales13@hotmail.fr>
2015-10-23 16:04:38 +0200

    Ajout de certaines choses pour le squelette
    Ajout des menus "Communauté" "Accueil" " Panel de modération" "Quêtes" "Panel d'administration" "Personnage" "Compte".

--- website/includes/nav.php
+++ website/includes/nav.php
@@ -2,8 +2,80 @@
 {
 ?>
     <nav>
+       <ul>
+            <h4 class="navhead">Accueil</h4>
+            <li><a href="rules.php">Règles</a></li>
+            <li><a href="server.php">Le Serveur</a></li>
+            <li><a href="candidature.php">Candidature</a></li>
+       </ul>
+    </nav>
+    
+    <nav>
+        <ul>
+            
+            <h4 class="navhead">Communauté</h4>
+            <li><a href="forum.php">Forums</a></li>
+            <li><a href="members.php">Membres</a></li>
+            <li><a href="factions.php">Factions</a></li>
+            <li><a href="guilds.php">Guildes</a></li>
+            <li><a href="chatbox.php">Chatbox</a></li>
+        </ul>
+    </nav>
+    
+     <nav>
+        <ul>
+            
+            <h4 class="navhead">Quêtes</h4>
+            <li><a href="quest.php">Mes quêtes</a></li>
+            <li><a href="questadmin.php">Administration des quêtes</a></li>
+             <li><a href="proposequest.php">Proposer une quête</a></li>
+        <ul>
+    </nav>
+    
+    <nav>
+        <ul>
+            
+            <h4 class="navhead">Personnage/h4>
+            <li><a href="perso.php">Mon Personnage</a></li>
+        <ul>
+    </nav>
+    
+        
+    <nav>
+        <ul>
+            
+            <h4 class="navhead">Panel de modération/h4>
+            <li><a href="staffrules.php">Règles du staff</a></li>
+            <li><a href="whitelist.php">Modération de la whitelist</a></li>
+        <ul>
+    </nav>
+    
+     <nav>
+        <ul>
+            
+            <h4 class="navhead">Panel d'administration</h4>
+            <li><a href="mjrules.php">Règles des Maîtres du Jeu</a></li>
+            <li><a href="cbmj.php">Chatbox MJ</a></li>
+            <li><a href="background.php">Background</a></li>
+            <li><a href="administration.php">Administration du Serveur</a></li>
+        <ul>
+    </nav>
+    
+     <nav>
+        <ul>
+            
+            <h4 class="navhead">Personnel</h4>
+            <li><a href="account.php">Mon Compte</a></li>
+            <li><a href="mp.php">Messages privés</a></li>
+            <li><a href="background.php">Background</a></li>
+            <li><a href="administration.php">Administration du Serveur</a></li>
+        <ul>
+    </nav>
+    
+    <nav>
         <ul>
             <li><a href="login.php">Connection</a></li>
+            <li><a href="disconnect.php"<Déconnection</a></li>
         </ul>
     </nav>
 <?php
