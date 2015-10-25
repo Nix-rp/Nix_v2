8e4e23a73a3aec792242743097a5f503988524ff
Aelerinya <sarah.stecklinck@gmail.com>
2015-10-25 14:36:48 +0100

    Structure home text with tags
    Réorganisation d ela structures pour que cela soit un peu mieux en html.

--- website/index.php
+++ website/index.php
@@ -11,19 +11,30 @@ html_header('acceuil');
 	<h2>Nix v2</h2>
 
 	<p>Version 2 de Nix, sources ici : <a href="https://github.com/Aelerinya/Nix_v2">Dépôt Github</a></p>
-	<p>Nix, qu'est-ce que c'est ?<br />
-Nix est un serveur dit &laquo; Roleplay &raquo; sur le thème d'une contrée enneigé. Nous pouvons aussi appeler ce type de jeu &laquo; Méta-Gaming &raquo;. Dans cette contrée, deux factions sont en guerre pour des raisons plus ou moins secrètes et obscures. Guidés par des êtres étranges.. et plus ou moins divins.. les membres de nos deux factions se lancent dans une guerre sanglante.. où il faudra être le plus stratégique possible. Chaque action compte. <br />
-Mais.. et si nos amis remortels n'étaient pas seuls en ce bas monde ? Et si.. il existait quelque chose.. &laquo; Plus haut &raquo; ?<br />
-Mais concrétement, qu'avez-vous de plus que les autres n'ont pas ?<br />
-Nix possède différents plugins codés par notre équipe de développeurs. Nous possédons un plugin de chat rp, permettant ainsi de pouvoir faire vivre au maximum votre personnage. Vous pourrez le faire crier, chuchoter, hurler, &laquo; télépather &raquo; ou communiquer avec un autre personnage par un moyen plus.. individuel. Les Maîtres du Jeu, grâce à ces outils, pourront vous offrir des évents de qualité sur le thème mystique.. ou de la guerre.<br />
-Et.. les grades ?<br />
-Nous avons un système de grades qui sont Hors Roleplay, c'est-à-dire qu'ils sont propres aux joueurs et non aux personnages. Votre personnage pourra bien sûr posséder un grade dans son institution ou sa guilde, qui lui sera inscrit dans les pages dédiés pour, mais les grades principaux, visibles sur le site, ce sera vous, le joueur qui serait concerné par ces échelons. Ils permettent de récompenser des actes faits et une implication conséquente. <br />
-Il existe 5 grades jouables, sur Nix.<br />
-Vous commencerez tout d'abord au rang de Nouveau, le rang noir. Pour pouvoir passer au rang gris, celui de joueur, vous devrez rédiger une candidature qui sera ou non acceptée par nos Modérateurs et Maîtres du Jeu. Dans le cas où elle serait acceptée, l'accès au serveur vous sera donné et vous pourrez découvrir notre contrée. Il est possible, après quelques mois d'activité et une envie d'en faire plus.. D'atteindre le grade de Joueur+, le grade bleu foncé. Ces personnes ont accès à des fonctions HRP comme la remises des avis positifs et négatifs du roleplay.. <br />
-Les joueurs qui s'investissent suffisamment pourront potentiellement être nommés au grade de Moderateur. Les Modérateurs, grade vert, ont accès à des fonctions de modération sur le site et le serveur, ils encadrent les joueurs pour différentes choses et aident les Maîtres du Jeu à la préparation des évents. Quand à ceux-ci, les Maîtres du Jeu, grade rouge.. Ils sont ceux qui gèrent la communauté. Ils encadrent les évents et créent du contenu, en cas de problème, vous pouvez vous adresser à eux.<br />
-Et maintenant, pour vous rejoindre ? <br />
-Pour nous rejoindre il ne vous reste que deux choses à faire. Il vous faut vous inscrire et rédiger votre candidature ! <br />
-</p>
+
+	<h4>Nix, qu'est-ce que c'est ?</h4>
+
+	<p>Nix est un serveur dit &laquo; Roleplay &raquo; sur le thème d'une contrée enneigé. Nous pouvons aussi appeler ce type de jeu &laquo; Méta-Gaming &raquo;. Dans cette contrée, deux factions sont en guerre pour des raisons plus ou moins secrètes et obscures. Guidés par des êtres étranges.. et plus ou moins divins.. les membres de nos deux factions se lancent dans une guerre sanglante.. où il faudra être le plus stratégique possible. Chaque action compte.</p>
+
+	<p>Mais.. et si nos amis remortels n'étaient pas seuls en ce bas monde ? Et si.. il existait quelque chose.. &laquo; Plus haut &raquo; ?</p>
+
+	<p>Mais concrétement, qu'avez-vous de plus que les autres n'ont pas ?</p>
+
+	<p>Nix possède différents plugins codés par notre équipe de développeurs. Nous possédons un plugin de chat rp, permettant ainsi de pouvoir faire vivre au maximum votre personnage. Vous pourrez le faire crier, chuchoter, hurler, &laquo; télépather &raquo; ou communiquer avec un autre personnage par un moyen plus.. individuel. Les Maîtres du Jeu, grâce à ces outils, pourront vous offrir des évents de qualité sur le thème mystique.. ou de la guerre.</p>
+
+	<p>Et.. les grades ?</p>
+
+	<p>Nous avons un système de grades qui sont Hors Roleplay, c'est-à-dire qu'ils sont propres aux joueurs et non aux personnages. Votre personnage pourra bien sûr posséder un grade dans son institution ou sa guilde, qui lui sera inscrit dans les pages dédiés pour, mais les grades principaux, visibles sur le site, ce sera vous, le joueur qui serait concerné par ces échelons. Ils permettent de récompenser des actes faits et une implication conséquente.</p>
+
+	<p>Il existe 5 grades jouables, sur Nix.</p>
+
+	<p>Vous commencerez tout d'abord au rang de Nouveau, le rang noir. Pour pouvoir passer au rang gris, celui de joueur, vous devrez rédiger une candidature qui sera ou non acceptée par nos Modérateurs et Maîtres du Jeu. Dans le cas où elle serait acceptée, l'accès au serveur vous sera donné et vous pourrez découvrir notre contrée. Il est possible, après quelques mois d'activité et une envie d'en faire plus.. D'atteindre le grade de Joueur+, le grade bleu foncé. Ces personnes ont accès à des fonctions HRP comme la remises des avis positifs et négatifs du roleplay..</p>
+
+	<p>Les joueurs qui s'investissent suffisamment pourront potentiellement être nommés au grade de Moderateur. Les Modérateurs, grade vert, ont accès à des fonctions de modération sur le site et le serveur, ils encadrent les joueurs pour différentes choses et aident les Maîtres du Jeu à la préparation des évents. Quand à ceux-ci, les Maîtres du Jeu, grade rouge.. Ils sont ceux qui gèrent la communauté. Ils encadrent les évents et créent du contenu, en cas de problème, vous pouvez vous adresser à eux.</p>
+
+	<p>Et maintenant, pour vous rejoindre ?</p>
+
+	<p>Pour nous rejoindre il ne vous reste que deux choses à faire. Il vous faut vous inscrire et rédiger votre candidature !</p>
 <?php
 html_footer();
 ?>
